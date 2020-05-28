1e89bd85803ec6289265b3fdb3ca85e0256f5eff
dave007700 <dave007700@gmail.com>
2020-05-28 11:41:09 +0200

    allow to use github actions

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const JazzUpdateSitePlugin = require('jazz-update-site-webpack-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
 const moment = require('moment');
 const packageJson = require('./package.json');
+const core = require('@actions/core');
 
 module.exports = (env) => {
 	const timestamp = moment().format('[_]YYYYMMDD[-]HHmm');
@@ -10,6 +11,9 @@ module.exports = (env) => {
 	const config = {
 		entry: {
 			app: './index.js'
+		},
+		output: {
+			filename: '[name]Bundle.js' // not used, prevent webpack from failing
 		},
 		plugins: [
 			new DisableOutputWebpackPlugin(),
@@ -48,6 +52,11 @@ module.exports = (env) => {
 			})
 		]
 	};
+
+	if (process.env["GITHUB_ACTIONS"]) {
+		// Set the output file name for use in GitHub Actions
+		core.setOutput("output_file", `${projectId}_${version}.zip`);
+	}
 
 	return config;
 };
